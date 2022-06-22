cb161ddcea45a97abd81fb0033820b3e8e4c1f50
Craig P Steffen <craigsteffen@gmail.com>
2022-06-22 12:41:43 -0400

    project name
    Changing the project name in the configuration file.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NCSA Nightingale'
+copyright = '2022, University of Illinois'
+author = 'NCSA'
 
 release = '0.1'
 version = '0.1.0'
